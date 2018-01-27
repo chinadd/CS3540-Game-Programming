3c9549ff5dfdf7b3d0b7657c976bfc5ef576b64e
Istiaque Shanjib <ishanjib@gmail.com>
2018-01-26 20:38:12 -0500

    assignment3

---
+++ assignment3/Planets/Assets/planet-custom/AABBGizmo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AABBGizmo : MonoBehaviour 
+{
+	private Bounds b;
+	private List<Bounds> bs;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+	}
+
+	void GetBounds(Transform child)
+	{
+		b.Encapsulate (child.GetComponent<Renderer> ().bounds);
+		if (child.childCount > 0) {
+			for (int i = 0; i < child.childCount; i++) {
+				GetBounds (child.GetChild (i));
+			}
+		}
+	}
+
+	void GetChildren(Transform child)
+	{
+		b = new Bounds (child.TransformPoint(child.position), Vector3.zero);
+		GetBounds (child);
+		bs.Add (b);
+		if (child.childCount > 0) {
+			for (int i = 0; i < child.childCount; i++) {
+				GetChildren(child.GetChild (i));
+			}
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		bs = new List<Bounds> ();
+		GetChildren(transform);
+		Gizmos.color = Color.white;
+		foreach (var a in bs) 
+		{
+			Gizmos.DrawWireCube (transform.position, a.size);
+		}
+	}
+}
