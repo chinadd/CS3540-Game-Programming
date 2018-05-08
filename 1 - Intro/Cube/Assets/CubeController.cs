b9d0fe00e98c2eb143b9b94b74ad9f8b8b8f04fa
Istiaque Shanjib <ishanjib@gmail.com>
2018-05-08 18:43:12 -0400

    rename folders

---
+++ 1 - Intro/Cube/Assets/CubeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Renderer> ().material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (Vector3.forward * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (Vector3.back * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (Vector3.left * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate (Vector3.right * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.Q)) {
+			transform.localScale = transform.localScale + Vector3.one * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.localScale = transform.localScale - Vector3.one * Time.deltaTime;
+		}
+	}
+}
